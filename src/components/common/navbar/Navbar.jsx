043cb5587cb268e04179761438eb19c85b675603
Shubham Pandey <shubhampandey1964@gmail.com>
2023-01-06 02:37:34 +0530

    updated:navbar responsiveness

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,28 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import {faMagnifyingGlass,faBars, faXmark, faChevronDown,} from "@fortawesome/free-solid-svg-icons";
+
 
 const Navbar = () => {
-  return (
-    <nav className='navbar'>
-      <ul className='left'>
-        <li className='item'>HOME</li>
-        <li className='item'>OUR INDUSTRIES</li>
-        <li className='item'>OUR COMPANIES</li>
-      </ul>
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
-      <ul className='right'>
-        <li>Career</li>
-        <li>Contact</li>
-        <li>
-          <FontAwesomeIcon
-            icon={faMagnifyingGlass}
-            className="search"
-          />
-        </li>
-      </ul>
-    </nav>
+  return (
+    <>  
+        
+        <nav className={isNavExpanded ? 'navbar-expanded': 'navbar'}>
+          <ul className='left'>
+            <li className='item'>HOME</li>
+            <li className='item'>
+              OUR INDUSTRIES 
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                className="arrowDown__icon"
+              />
+            </li>
+            <li className='item'>
+              OUR COMPANIES
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                className="arrowDown__icon"
+              />
+            </li>
+          </ul>
+
+          <ul className='right'>
+            <li className='item'>Career</li>
+            <li className='item'>Contact</li>
+            <li>
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className="search item"
+              />
+            </li>
+          </ul>
+        </nav>
+        {
+
+          isNavExpanded 
+          ? (
+              <FontAwesomeIcon
+                icon={faXmark}
+                className="hamburger"
+                style={{"width": "14px"}}
+                onClick={() => {
+                  setIsNavExpanded(!isNavExpanded);
+                }}
+              />
+            )
+          : (
+            <FontAwesomeIcon
+                icon={faBars}
+                className="hamburger"
+                onClick={() => {
+                  setIsNavExpanded(!isNavExpanded);
+                }}
+            />
+          )  
+        }
+    </>
   )
 }
 
